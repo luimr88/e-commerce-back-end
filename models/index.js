7f291c328108d6830817c18ec0a3468fdf5c60a0
Luis Riveros <luisriveros26@gmail.com>
2022-06-11 10:29:10 -0600

    finished model relationships

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Product');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -13,8 +14,15 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
